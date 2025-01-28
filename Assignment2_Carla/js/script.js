2e538fce2d3ad4d8583bb3d6d5371eca7c74fe15
Carla Melody <165509577+carlamelody@users.noreply.github.com>
2025-01-27 21:53:04 -0800

    Add files via upload

---
+++ Assignment2_Carla/js/script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById('myButton');
+const contentDiv = document.getElementById('content');
+
+function showImage() {
+    contentDiv.innerHTML = '<img src="images/boo.jpg" id="booImage">';
+    button.textContent = "Make it go away!";
+    button.removeEventListener('click', showImage);
+    button.addEventListener('click', hideImage);
+}
+
+function hideImage() {
+    contentDiv.innerHTML = '';
+    button.textContent = "Don't Click Me!";
+    button.removeEventListener('click', hideImage);
+    button.addEventListener('click', showImage);
+}
+
+button.addEventListener('click', showImage);
